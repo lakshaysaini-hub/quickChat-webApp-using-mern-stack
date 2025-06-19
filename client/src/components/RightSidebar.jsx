136c0d5cb0159e754f8c2128a71321c770e93802
Lakshay <lakshaysaini05401iclod.com@Lakshays-MacBook-Air-2.local>
2025-06-20 01:22:23 +0530

    right sidebar structure dev

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import assets from "../assets/assets";
 
-const RightSidebar = () => {
-  return <div></div>;
+const RightSidebar = ({ selectedUser }) => {
+  return (
+    selectedUser && (
+      <div
+        className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${
+          selectedUser ? "max-md:hidden" : ""
+        }`}
+      >
+        {/* user icon & bio */}
+        <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt="User-Avatar"
+            className="w-20 aspect-square rounded-full"
+          />
+          <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
+            <span className="w-2 h-2 rounded-full bg-green-500"></span>
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
+
+        {/* horizonatal line */}
+        <hr className="border-[#ffffff50] my-4" />
+
+        {/* media */}
+        <div></div>
+      </div>
+    )
+  );
 };
 
 export default RightSidebar;
